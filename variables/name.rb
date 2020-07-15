8678255b40d8cde0316865cf95c5d76c781eafdd
Drea Hernandez <dvhdez@gmail.com>
2020-07-15 01:59:29 +0000

    Variables chapter exercises

---
+++ variables/name.rb
@@ -0,0 +1,9 @@
+name = gets.chomp
+puts "Hello, " + name
+10.times {puts name}
+
+puts "What is your first name?"
+first_name = gets.chomp
+puts "Thank you. What is your last name?"
+last_name = gets.chomp
+puts "Great. So your full name is " + first_name + " " + last_name
